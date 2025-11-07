a823f1e85cc792eea9f6cb5296d16ddafdec8d3f
bdeischer <156730926+bdeischer@users.noreply.github.com>
2025-11-07 13:00:27 -0700

    Delete AMISYS/Active_Member_Count.sql

--- AMISYS/Active_Member_Count.sql
+++
@@ -1,6 +0,0 @@
-- Active Member Count as of today
-
-select count(distinct member_nbr)
-from member_span ms
-where to_char(sysdate, 'yyyymmdd') between substr(ymdeff,1,4) and substr(ymdend,1,4)
-and void = ' ';
