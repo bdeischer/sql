2d9f65072dc1e687c4b010512ab57cb17200784f
bdeischer <156730926+bdeischer@users.noreply.github.com>
2025-11-19 06:50:30 -0700

    Update ClaimCountbyExcodeCombination_V11.sql

--- AMISYS/claim/ClaimCountbyExcodeCombination_V11.sql
+++ AMISYS/claim/ClaimCountbyExcodeCombination_V11.sql
@@ -1,5 +1,8 @@
-var pymdpaid number;
-exec :pymdpaid := 20251101;
+var begin_ymdpaid number;
+var end_ymdpaid number;
+
+exec :begin_ymdpaid := 20251101;
+exec :end_ymdpaid := 20251130;
 
 select b.explain_codes, 
        b.explain_code_desc,
@@ -14,7 +17,7 @@ as
      inner join service_x sx
      on substr(ph.record_key,3,18) = sx.serv_nbr
      and substr(sx.serv_nbr,13,6) = '000100'
-     and sx.ymdpaid > :pymdpaid
+     and sx.ymdpaid between :begin_ymdpaid and :end_ymdpaid
      inner join amiown.explain_code ec1
      on ec1.explain_x in(substr(ph.free_form_data,16,4), substr(ph.free_form_data,20,4), substr(ph.free_form_data,24,4), substr(ph.free_form_data,28,4),substr(ph.free_form_data,32,4),substr(ph.free_form_data,36,4))
      and ec1.serv_pay_status = 'PEND'
@@ -25,7 +28,7 @@ as
      inner join service_x sx2
      on substr(ph.record_key,3,18) = sx2.serv_nbr
      and substr(sx2.serv_nbr,13,6) = '000100'
-     and sx2.ymdpaid > :pymdpaid
+     and sx2.ymdpaid between :begin_ymdpaid and :end_ymdpaid
      inner join amiown.explain_code ec2
      on ec2.explain_x in(substr(ph.free_form_data,16,4), substr(ph.free_form_data,20,4), substr(ph.free_form_data,24,4), substr(ph.free_form_data,28,4),substr(ph.free_form_data,32,4),substr(ph.free_form_data,36,4))
      and ec2.serv_pay_status = 'PEND'
@@ -36,7 +39,7 @@ as
      inner join service_x sx3 
      on substr(ph.record_key,3,18) = sx3.serv_nbr
      and substr(sx3.serv_nbr,13,6) = '000100'
-     and sx3.ymdpaid > :pymdpaid
+     and sx3.ymdpaid between :begin_ymdpaid and :end_ymdpaid
      inner join amiown.explain_code ec3
      on ec3.explain_x in(substr(ph.free_form_data,16,4), substr(ph.free_form_data,20,4), substr(ph.free_form_data,24,4), substr(ph.free_form_data,28,4),substr(ph.free_form_data,32,4),substr(ph.free_form_data,36,4))
      and ec3.serv_pay_status = 'PEND'
@@ -47,7 +50,7 @@ as
      inner join service_x sx4
      on substr(ph.record_key,3,18) = sx4.serv_nbr
      and substr(sx4.serv_nbr,13,6) = '000100'
-     and sx4.ymdpaid > :pymdpaid
+     and sx4.ymdpaid between :begin_ymdpaid and :end_ymdpaid
      inner join amiown.explain_code ec4
      on ec4.explain_x in(substr(ph.free_form_data,16,4), substr(ph.free_form_data,20,4), substr(ph.free_form_data,24,4), substr(ph.free_form_data,28,4),substr(ph.free_form_data,32,4),substr(ph.free_form_data,36,4))
      and ec4.serv_pay_status = 'PEND'
@@ -58,7 +61,7 @@ as
      inner join service_x sx5
      on substr(ph.record_key,3,18) = sx5.serv_nbr
      and substr(sx5.serv_nbr,13,6) = '000100'
-     and sx5.ymdpaid > :pymdpaid
+     and sx5.ymdpaid between :begin_ymdpaid and :end_ymdpaid
      inner join amiown.explain_code ec5
      on ec5.explain_x in(substr(ph.free_form_data,16,4), substr(ph.free_form_data,20,4), substr(ph.free_form_data,24,4), substr(ph.free_form_data,28,4),substr(ph.free_form_data,32,4),substr(ph.free_form_data,36,4))
      and ec5.serv_pay_status = 'PEND'
@@ -69,7 +72,7 @@ as
      inner join service_x sx6
      on substr(ph.record_key,3,18) = sx6.serv_nbr
      and substr(sx6.serv_nbr,13,6) = '000100'
-     and sx6.ymdpaid > :pymdpaid
+     and sx6.ymdpaid between :begin_ymdpaid and :end_ymdpaid
      inner join amiown.explain_code ec6
      on ec6.explain_x in(substr(ph.free_form_data,16,4), substr(ph.free_form_data,20,4), substr(ph.free_form_data,24,4), substr(ph.free_form_data,28,4),substr(ph.free_form_data,32,4),substr(ph.free_form_data,36,4))
      and ec6.serv_pay_status = 'PEND'
@@ -84,3 +87,4 @@ group by claim_nbr
 ) b
 group by explain_codes, explain_code_desc
 ;     
+ 
